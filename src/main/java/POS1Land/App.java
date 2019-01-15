ac531ff279b16cb5ab395a5182d8483568b64015
Basti098765 <omgitstheoesi@gmail.com>
2019-01-15 19:39:13 +0100

    Init

--- src/main/java/POS1Land/App.java
+++ src/main/java/POS1Land/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        WR e2d = new Euro2Dollar();
+        WR e2y = new Euro2Yen();
+        e2d.setNextCalc(e2y);
+
+
+        System.out.println( e2d.umrechnen("Euro2Dollar",20));
+        System.out.println(e2y.umrechnen("Euro2Yen",10));
     }
 }
--- src/main/java/POS1Land/WR.java
+++ src/main/java/POS1Land/WR.java
@@ -4,13 +4,21 @@ public abstract class WR implements IUmrechnen
 {
     protected WR nextCalc;
     protected double kurs;
+    protected String var;
+
 
     public void setNextCalc(WR nextCalc)
     {
         this.nextCalc=nextCalc;
     }
     public double umrechnen(String variante, double betrag) {
-
+        if(this.var==variante)
+        {
+            return kurs*betrag;
+        }if(nextCalc!=null)
+        {
+            nextCalc.umrechnen(variante,betrag);
+        }
        return betrag*kurs;
 
 }
